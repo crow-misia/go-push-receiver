c69086346924c5145aff0633a85de6d2cfcd715b
crow-misia <2736571+crow-misia@users.noreply.github.com>
2025-10-14 02:09:12 +0000

    ci: apply formatting changes
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pb/checkin/checkin.pb.go
+++ pb/checkin/checkin.pb.go
@@ -13,11 +13,12 @@
 package checkin_proto
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
